88556e90313096d0e9962c41cca30ac14423c8ae
2peng23 <aryayouh@gmail.com>
2023-11-20 12:20:50 +0800

    commit

---
+++ resources/views/default/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.user')
+@section('content')
+    @include('default.slider')
+
+
+    @include('default.feature')
+
+
+
+    @include('default.about')
+
+
+    @include('default.service')
+
+
+    @include('default.feature2')
+
+
+    @include('default.project')
+@endsection
