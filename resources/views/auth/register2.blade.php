aacedce1b2d1b7bd7e83eccbb0fb6884f3e6367e
2peng23 <aryayouh@gmail.com>
2023-11-21 10:56:17 +0800

    commit

--- resources/views/auth/register2.blade.php
+++ resources/views/auth/register2.blade.php
@@ -10,9 +10,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
-        .div1 {
+        .div2 {
             /* Set the background image */
-            background-image: url('content/Cakes/drip/1.png');
+            background-image: url('content/Cakes/drip/13.png');
 
             /* Specify background properties */
             background-size: cover;
@@ -24,8 +24,10 @@
 
 <body>
     <div class="container-fluid">
-        <div class="row min-vh-100 " id="div1"> <!-- Add a custom class for styling -->
-            <div class="col-lg-8 col-md-6 div1 full-height d-none d-md-block "> <!-- Add a custom class for styling -->
+
+
+        <div class="row min-vh-100 " id="div2"> <!-- Add a custom class for styling -->
+            <div class="col-lg-8 col-md-6 div2 full-height d-none d-md-block "> <!-- Add a custom class for styling -->
                 {{-- <img src="content/styling-images/1.jpg" alt=""> --}}
             </div>
             <div class="col-md-6 col-lg-4 h-100"> <!-- Add a custom class for styling -->
@@ -35,26 +37,37 @@
                             <img src="content/logo/b-logo.png" alt="" style="width: 200px">
                         </a>
                     </div>
-                    <h5 class="text-center fw-bolder fs-1 pb-4 text-muted">Welcome Back</h5>
+                    <h5 class="text-center fw-bolder fs-1 pb-4 text-muted">Create Your Account</h5>
                     <form>
                         <div class="form-group mb-4">
-                            <label class="mb-2" for="username">Email Address <span
+                            <label class="mb-2" for="Name">Name <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control py-2 px-3" style="border-radius: 20px"
+                                id="Name" placeholder="Enter Full Name">
+                        </div>
+                        <div class="form-group mb-4">
+                            <label class="mb-2" for="email">Email Address <span
                                     class="text-danger">*</span></label>
                             <input type="text" class="form-control py-2 px-3" style="border-radius: 20px"
-                                id="username" placeholder="Enter email address">
+                                id="email" placeholder="Enter email address">
                         </div>
                         <div class="form-group mb-4">
                             <label class="mb-2" for="password">Password <span class="text-danger">*</span></label>
                             <input type="password" class="form-control py-2 px-3" style="border-radius: 20px"
                                 id="password" placeholder="Enter your password">
                         </div>
+                        <div class="form-group mb-4">
+                            <label class="mb-2" for="password_confirmation">Password Confirmation <span
+                                    class="text-danger">*</span></label>
+                            <input type="text" class="form-control py-2 px-3" style="border-radius: 20px"
+                                id="password_confirmation" placeholder="Confirm Password">
+                        </div>
                         <button type="submit" class="btn btn-primary py-2 px-3"
-                            style="border-radius: 20px">Login</button>
+                            style="border-radius: 20px">Register</button>
                     </form>
 
-                    <p class="mt-5 fs-6">Don't have an account yet? <a href="/register"
-                            class="fw-bold text-decoration-none text-primary" style="cursor: pointer">Register</a>
-                        for free
+                    <p class="mt-5 fs-6">Already have an account? <a href="/login"
+                            class="fw-bold text-decoration-none text-primary" style="cursor: pointer">Login</a>
+                        now!
                     </p>
                 </div>
             </div>
